e60372b41a5bd601196ea3ced0b6a8ea647768c1
Connie <cleung334@gmail.com>
2017-08-29 06:12:08 +0800

    javascript30 - Complete day 29. Countdown Timer

--- 29-countdown-timer/scripts-START.js
+++ 29-countdown-timer/scripts-START.js
@@ -2,8 +2,11 @@ let countdown;
 
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
   console.log(now, then);
@@ -39,3 +42,18 @@ function displayEndTime(timestamp) {
     const minutes = end.getMinutes();
     endTime.textContent = `Be back at ${adjustedHour}:${minutes < 10 ? 0 : ''}${minutes}`;
 }
+
+function startTimer() {
+  console.log(this.dataset.time);
+  timer(this.dataset.time);
+}
+
+console.log(buttons);
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+});
