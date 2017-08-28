921824ca601ca4b5d87b5f8779361d572052ff80
Connie <cleung334@gmail.com>
2017-08-28 08:05:52 +0800

    javascript30 - Complete 70% of the exercise 29 - countdown timer

---
+++ 29-countdown-timer/scripts-START.js
@@ -0,0 +1,41 @@
+let countdown;
+
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  console.log(now, then);
+
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+      const secondsLeft = Math.round( (then - Date.now()) / 1000 );
+      // check if we should stop it
+      if (secondsLeft < 0) {
+        clearInterval (countdown);
+        return;
+      }
+      // display it
+      displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? 0 : ''}${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+  console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be back at ${adjustedHour}:${minutes < 10 ? 0 : ''}${minutes}`;
+}
